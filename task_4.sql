87ddf27b075ead1140b6c058218f29206dfd0025
Ivy-Mumbi <ivy.mumbi.5@gmail.com>
2025-10-24 18:35:28 +0300

    Revert "Update4"
    This reverts commit 577196d3f892924bf02416a9bfc68a94b160560a.

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,3 @@
--- task_4.sql
--- Objective: Print the full description of the table 'books' without using DESCRIBE or EXPLAIN
-
-USE alx_book_store;
-
-SHOW CREATE TABLE books;
-
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'books';
