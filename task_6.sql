36a8fdbe72bbe6f25031fc8d73dd002e8319c3b2
Ivy-Mumbi <ivy.mumbi.5@gmail.com>
2025-10-24 18:53:39 +0300

    Update 6

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,10 @@
 -- task_6.sql
 -- This script inserts multiple rows into the table 'customer'
--- in the database 'alx_book_store'
+-- in the database passed as an argument to the mysql command
 
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+
